bbf52a5b1a5b556470e28795463c65dd4bb12738
butara <78299877+butara@users.noreply.github.com>
2021-02-17 02:41:44 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,33 @@
+<?php
+
+$url = "xxxx";
+
+$trace = "xxxxxxx";
+
+$user = "xxxxxxxx";
+
+
+
+$data2 = 'xxxxxx';
+
+$data2 = 'xxxxxx';
+
+$data3 = 'xxxxxxx';
+
+$data4 = 'xxxxxxx';
+
+$data5 = 'xxxxxxx';
+
+$data6 = 'xxxxxxx';
+
+$data7 = 'xxxxxxx';
+
+$data8 = 'xxxxxxx';
+
+$data9 = 'xxxxxxx';
+
+$data10 = 'xxxxxxx';
+
+$data11 = 'xxxxxxx';
+
+$data12 = 'xxxxxxx';
